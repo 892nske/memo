223ba92b5772005be8cbf0f95db9c18a3148bb08
892nske <tomo.kadokura@gmail.com>
2020-03-15 22:40:13 +0900

    My Django Girls app, first commit

---
+++ app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Memo
+from django.shortcuts import get_object_or_404
+from .forms import MemoForm
+
+
+def index(request):
+    memos = Memo.objects.all().order_by('-updated_datetime')
+    return render(request, 'app/index.html', {'memos': memos})
+
+
+def detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    return render(request, 'app/detail.html', {'memo': memo})
+
+
+def new_memo(request):
+    if request.method == "POST":
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app:index')
+    else:   
+        form = MemoForm
+    return render(request, 'app/new_memo.html', {'form': form})
